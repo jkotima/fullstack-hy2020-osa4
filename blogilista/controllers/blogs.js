705e08461f50183dad63e9da5a58d3239bed6e26
jkotima <jkotima@gmail.com>
2020-07-14 13:27:30 +0300

    4.20

--- blogilista/controllers/blogs.js
+++ blogilista/controllers/blogs.js
@@ -11,21 +11,12 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-const getTokenFrom = request => {
-  const authorization = request.get('Authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const token = getTokenFrom(request)
-  console.log(token)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
+  console.log(request.token)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
 
-  if (!token || !decodedToken.id) {
+  if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
@@ -48,7 +39,6 @@ blogsRouter.post('/', async (request, response) => {
   await user.save()
 
   response.json(savedBlog.toJSON())
-  
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
